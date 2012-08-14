79ace81cccf5777e605093994a8c7a4e44f4e1f7
Jari Rosti <jari.rosti@syslore.com>
2012-08-14 18:49:01 +0300

    importing training logs now works with structured reports

--- src/ontrail/import.clj
+++ src/ontrail/import.clj
@@ -6,9 +6,13 @@
 
 (def import-html (html/html-resource (java.io.FileReader. "peppi-trainlog.html")))
 
-(def exs (rest (html/select import-html [[:tr]])))
+(def exs (rest (html/select import-html [:tr])))
+
+(defn is-ex? [ex-html]
+	(= 10 (count (get ex-html :content))))
+
 (defn get-ex [ex-html]
-	(html/select ex-html [:td]))
+	(get ex-html :content))
 
 (defn convert-to-timestamp [date-string]
 	(let [date-array (map #(read-string (str "10r" %)) (clojure.string/split date-string #"\."))]
@@ -37,9 +41,15 @@
 	(let [distance-string (html/text (nth ex 5))]
 		(int (* 1000 (read-string (clojure.string/replace distance-string #"," "."))))))
 
+(defn to-duration [duration-string]
+	(try
+		(int (* 6000 (read-string duration-string)))
+	(catch Exception e 
+		(0))))
+
 (defn get-duration [ex]
 	(let [duration-string (html/text (nth ex 4))]
-		(int (* 6000 (read-string duration-string)))))
+		(to-duration duration-string)))
 
 (defn get-avghr [ex]
 	(int (read-string (html/text (nth ex 6)))))
@@ -69,5 +79,5 @@
 
 (defn -main []
 	(try
-		(map #(insert (get-uid "peppi") (get-ex %)) exs)
+		(map #(if (is-ex? %) (insert (get-uid "peppi") (get-ex %))) exs)
 	(catch Exception e (prn e))))
--- test/ontrail/test/import.clj
+++ test/ontrail/test/import.clj
@@ -2,5 +2,8 @@
   (:use [ontrail.import])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest basic-duration
+  (is (= 360000 (to-duration "60.0")) "Basic duration"))
+
+(deftest with-hundreds-duration
+  (is (= 6001 (to-duration "1.0,1")) "Hundreds"))
