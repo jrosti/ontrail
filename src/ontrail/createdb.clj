c42ea46db4a0f991921d00b8d7f74b15251b4868
Jari Rosti <jari.rosti@syslore.com>
2012-08-15 11:18:43 +0300

    reformatted

--- src/ontrail/createdb.clj
+++ src/ontrail/createdb.clj
@@ -5,43 +5,43 @@
 (def db (System/getenv "DATABASE_URL"))
 
 (def create-exs (sql/with-connection db
-		(sql/create-table :exs
-			[:id :serial "PRIMARY KEY"]
-			[:date :timestamp] 
-			[:uid :integer]
-			[:sport :integer]
-			[:duration :integer]
-			[:distance :integer]
-			[:avghr :integer]
-			[:elevation :integer]
-			[:heading "varchar(128)"]
-			[:report :text]
-			[:tags :text]
-			[:ugoperm :integer]
-			)))
+        (sql/create-table :exs
+            [:id :serial "PRIMARY KEY"]
+            [:date :timestamp] 
+            [:uid :integer]
+            [:sport :integer]
+            [:duration :integer]
+            [:distance :integer]
+            [:avghr :integer]
+            [:elevation :integer]
+            [:heading "varchar(128)"]
+            [:report :text]
+            [:tags :text]
+            [:ugoperm :integer]
+            )))
 
 (def create-sports (sql/with-connection db
-	(doall [(sql/create-table :sport
-			[:id :serial]
-			[:name "varchar(30) unique" "PRIMARY KEY"] 
-			[:description :text])
-			(sql/insert-values :sport [:id :name] [0 "Muu laji"])
-			(sql/insert-values :sport [:name] ["Juoksu"])
-			(sql/insert-values :sport [:name] ["Pyöräily"])])))
-		
+    (doall [(sql/create-table :sport
+            [:id :serial]
+            [:name "varchar(30) unique" "PRIMARY KEY"] 
+            [:description :text])
+            (sql/insert-values :sport [:id :name] [0 "Muu laji"])
+            (sql/insert-values :sport [:name] ["Juoksu"])
+            (sql/insert-values :sport [:name] ["Pyöräily"])])))
+        
 (def create-users (sql/with-connection db
-	(doall	[(sql/create-table :onuser
-			[:id :serial]
-			[:name "varchar(20) unique"] 
-			[:goals :text]
-			[:pwdhash "varchar(20)"])
-			(sql/insert-values :onuser [:name] ["peppi"])])))	
+    (doall  [(sql/create-table :onuser
+            [:id :serial]
+            [:name "varchar(20) unique"] 
+            [:goals :text]
+            [:pwdhash "varchar(20)"])
+            (sql/insert-values :onuser [:name] ["peppi"])])))   
 
 (def create-group (sql/with-connection db
-	(doall 	[(sql/create-table :ongroup
-			[:id :serial]
-			[:name "varchar(40) unique"] 
-			[:description :text])])))
+    (doall  [(sql/create-table :ongroup
+            [:id :serial]
+            [:name "varchar(40) unique"] 
+            [:description :text])])))
 
 (defn -main []
-	(doall [create-exs create-sports create-users create-group]))
+    (doall [create-exs create-sports create-users create-group]))
--- src/ontrail/import.clj
+++ src/ontrail/import.clj
@@ -9,79 +9,79 @@
 (def exs (rest (html/select import-html [:tr])))
 
 (defn is-ex? [ex-html]
-	(= 10 (count (get ex-html :content))))
+    (= 10 (count (get ex-html :content))))
 
 (defn get-ex [ex-html]
-	(get ex-html :content))
+    (get ex-html :content))
 
 (defn convert-to-timestamp [date-string]
-	"Input format: 12.08.2012"
-	(let [date-array (map #(read-string (str "10r" %)) (clojure.string/split date-string #"\."))]
-		(java.sql.Timestamp. (.getTime (java.util.Date. (- (nth date-array 2) 1900) 
-														(+ 1 (second date-array)) 
-														(first date-array) 12 0)))))
+    "Input format: 12.08.2012"
+    (let [date-array (map #(read-string (str "10r" %)) (clojure.string/split date-string #"\."))]
+        (java.sql.Timestamp. (.getTime (java.util.Date. (- (nth date-array 2) 1900) 
+                                                        (+ 1 (second date-array)) 
+                                                        (first date-array) 12 0)))))
 
 (defn get-uid [name] 
-	(sql/with-connection db
-		(sql/with-query-results results ["select id from onuser where name = ?" name] 
-			(get (first results) :id))))
+    (sql/with-connection db
+        (sql/with-query-results results ["select id from onuser where name = ?" name] 
+            (get (first results) :id))))
 
 (defn get-timestamp [ex] (convert-to-timestamp (html/text (nth ex 0))))
 (defn get-heading [ex] (html/text (nth ex 1)))
 
 (defn get-sport [ex] 
-	(let [sport (html/text (nth ex 2))]
-		(sql/with-connection db 
-			(sql/with-query-results results ["select id from sport where name = ?" sport]
-			 (if (> (count results) 0) (get (first results) :id) 0)))))
+    (let [sport (html/text (nth ex 2))]
+        (sql/with-connection db 
+            (sql/with-query-results results ["select id from sport where name = ?" sport]
+             (if (> (count results) 0) (get (first results) :id) 0)))))
 
 
 (defn get-report [ex] (html/text (nth ex 3)))
 
 (defn get-distance [ex]
-	"Returns meters. Input: 42,195km"
-	(let [distance-string (html/text (nth ex 5))]
-		(int (* 1000 (read-string (clojure.string/replace distance-string #"," "."))))))
+    "Returns meters. Input: 42,195km"
+    (let [distance-string (html/text (nth ex 5))]
+        (int (* 1000 (read-string (clojure.string/replace distance-string #"," "."))))))
 
 (defn to-duration [duration-string]
-	"Input format: 14 400,12 is 14400.12 minutes"
-	(let [duration-dotted (clojure.string/replace duration-string #"," ".")
-		  minutes (read-string (clojure.string/replace duration-dotted " " ""))]
-		  (int (* 6000 (read-string duration-string)))))
+    "Input format: 14 400,12 is 14400.12 minutes"
+    (let [duration-dotted (clojure.string/replace duration-string #"," ".")
+          minutes (read-string (clojure.string/replace duration-dotted " " ""))]
+          (int (* 6000 (read-string duration-string)))))
 
 (defn get-duration [ex]
-	"Unit: 1/100 seconds integer"
-	(let [duration-string (html/text (nth ex 4))]
-		(to-duration duration-string)))
+    "Unit: 1/100 seconds integer"
+    (let [duration-string (html/text (nth ex 4))]
+        (to-duration duration-string)))
 
 (defn get-avghr [ex]
-	(int (read-string (html/text (nth ex 6)))))
+    (int (read-string (html/text (nth ex 6)))))
 
 (defn get-tags [ex]
-	(html/text (nth ex 8)))
+    (html/text (nth ex 8)))
 
 (defn insert-sql [uid timestamp heading report duration distance avghr tags sport] 
-	(sql/with-connection db
-		(sql/insert-values :exs
-			[:uid :date :heading :report :duration :distance :avghr :tags :sport] 
-			[uid timestamp heading report duration distance avghr tags sport])))
+    (sql/with-connection db
+        (sql/insert-values :exs
+            [:uid :date :heading :report :duration :distance :avghr :tags :sport] 
+            [uid timestamp heading report duration distance avghr tags sport])))
 
 (defn insert [uid excercise]
-	(dorun [(try
-				(insert-sql uid
-					(get-timestamp excercise) 
-					(get-heading excercise) 
-					(get-report excercise) 
-					(get-duration excercise)
-					(get-distance excercise)
-					(get-avghr excercise)
-					(get-tags excercise)
-					(get-sport excercise))
-			(catch Exception e 
-				(prn uid e excercise)))]))
+    (dorun [(try
+                (insert-sql uid
+                    (get-timestamp excercise) 
+                    (get-heading excercise) 
+                    (get-report excercise) 
+                    (get-duration excercise)
+                    (get-distance excercise)
+                    (get-avghr excercise)
+                    (get-tags excercise)
+                    (get-sport excercise))
+            (catch Exception e 
+                (prn uid e excercise)))]))
 
 (defn -main []
-	(let [uid (get-uid "peppi")]
-		(try
-			(doall (map #(if (is-ex? %) (insert uid (get-ex %))) exs))
-		(catch Exception e (prn e)))))
+    (let [uid (get-uid "peppi")]
+        (try
+            (doall (map #(if (is-ex? %) (insert uid (get-ex %))) exs))
+        (catch Exception e (prn e)))))
