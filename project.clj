b26b8540b4fdad40064489eb3a7c5279ae2eb0ec
Jari Rosti <jari.rosti@syslore.com>
2012-08-18 12:29:07 +0300

    defined webapp routing similar way as clojure-aleph-chat

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject ontrail "0.0.1"
   :description "Logs on my trails"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aleph "0.3.0-alpha3"]
-                 [ring/ring-core "[0.2.0,)"]
-                 [net.cgrand/moustache "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [aleph "0.3.0-alpha2"]
+                 [compojure "1.1.1"]
+                 [ring "1.1.0-beta2"]
+                 [hiccup "1.0.0-beta1"]
+                 [lein-swank "1.4.4"]
                  [enlive "1.0.0"]
-		 [lamina "0.5.0-alpha4"]
-		 [sandbar/sandbar "0.3.0"]
-                 [com.novemberain/monger "1.1.2"]
-		 [org.clojure/data.json "0.1.3"]]
+                 [org.clojure/data.json "0.1.3"]
+                 [com.novemberain/monger "1.1.2"]]
   :run-aliases {:import ontrail.import}
   :main ontrail.core)
 
--- src/ontrail/core.clj
+++ src/ontrail/core.clj
@@ -1,33 +1,86 @@
-(ns ontrail.core)
+(ns ontrail.core
+  (:use lamina.core
+        aleph.http
+        compojure.core
+        ; This sets the correct file type for js includes used by hiccup
+        (ring.middleware resource file-info)
+        (hiccup core page))
+  (:require [compojure.route :as route])
+  (:gen-class))
 
 (require '[monger.collection :as mc])
 (require '[clojure.string])
 
 (use '[clojure.data.json :only (read-json json-str)])
-(use 'aleph.http)
-(use 'net.cgrand.moustache)
-(use 'ontrail.mongodb)
-(use 'lamina.core)
 
-(defn async-handler [response-channel request]
-  (enqueue response-channel
-    {:status 200
-     :headers {"content-type" "text/plain"}
-     :body "async response"}))
+(defn chat-init [ch]
+  "Initialize a new chat channel"
+  (receive-all ch #(println "message: " %)))
 
-(defn get-user-info [path-info]
-  (let [user-id (clojure.string/replace path-info #"^/" "")]
-    (json-str (mc/find-one "onuser" {:_id user-id}))))
+(defn chat-handler [ch room]
+  "Relays messages into a chat room. If it doesn't
+  exist create a new channel"
+  (let [chat (named-channel room chat-init)]
+    (siphon chat ch)
+    (siphon ch chat)))
 
-(defn user-handler [req]
+(defn page []
+  "HTML page rendered using Hiccup. Includes the css and js for websockets."
+  (html5
+   [:head
+    (include-css "/static/stylesheets/master.css")]
+   [:body
+    [:div.container
+     [:div.row
+      [:div.columns.twelve
+        [:p [:h1#headline "Chat"]]
+        [:form
+          [:input#message {:type "text"}]
+          [:input.nice.large.blue.button {:type "submit"}]]
+        [:div#messages]]]]
+    (include-js "http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
+    (include-js "/static/javascripts/web_socket.js")
+    (include-js "/static/javascripts/app.js")]))
+
+(defn sync-app [request]
+  "Rendered response of the chat page"
+  {:status 200
+   :headers {"content-type" "text/html"}
+   :body (page)})
+
+
+(def wrapped-sync-app
+  "Wraps the response with static files"
+  (-> sync-app
+      (wrap-resource "public")
+      (wrap-file-info)))
+
+(defn chat [ch request]
+  "View handler that handles a chat room. If it's not
+  a websocket request then return a rendered html response."
+  (let [params (:route-params request)
+        room (:room params)]
+    (if (:websocket request)
+      (chat-handler ch room)
+      (enqueue ch (wrapped-sync-app request)))))
+
+(def user-handler
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    (get-user-info (get req :path-info))})
+   :body    "Hello"})
 
-(def handler 
-  (app 
-    ["user" &] user-handler
-    ["async"] {:get (wrap-aleph-handler async-handler)}))
+(defroutes app-routes
+  "Routes requests to their handler function. Captures dynamic variables."
+  (GET ["/chat/:room", :room #"[a-zA-Z]+"] {}
+       (wrap-aleph-handler chat))
+  (GET ["/"] {} user-handler)
+  ;;Route our public resources like css and js to the static url
+  (route/resources "/static")
+  ;;Any url without a route handler will be served this response
+  (route/not-found "Page not found"))
 
-(defn -main []
-  (start-http-server (wrap-ring-handler handler) {:port 8080}))
+(defn -main [& args]
+  "Main thread for the server which starts an async server with
+  all the routes we specified and is websocket ready."
+  (start-http-server (wrap-ring-handler app-routes)
+                     {:host "localhost" :port 8080 :websocket true}))
