5fef093a36b76ea04bd0eae85fedd7b85c41c720
Jari Rosti <jari.rosti@syslore.com>
2012-08-19 12:08:42 +0300

    added average heart rate to summary

--- src/ontrail/queries.clj
+++ src/ontrail/queries.clj
@@ -3,11 +3,10 @@
         [monger.result :only [ok?]]
         [monger.conversion :only [from-db-object]]))
 
-(use 'ontrail.mongodb)
+(require '[clj-time.core :as time])
+(require '[monger.joda-time])
 
-;; Pace computation uses exercises, where both distance and duration are known. Those sums are recorder to tdur and tdist
-;; dist and dur are plain sums over the db values.
-(def js-reduce "function(obj, prev) { if (obj.distance > 0 && obj.duration > 0) { prev.tdist += obj.distance; prev.tdur += obj.duration; } prev.dist += obj.distance; prev.dur += obj.duration }")
+(use 'ontrail.mongodb)
 
 (defn to-human-distance [distance]
   (let [km (int (/ distance 1000))
@@ -21,27 +20,58 @@
         hours (int (/ duration 360000))]
     (str hours "h " minutes "min")))       
 
+(defn to-human-pace [pace]
+  (let [secs (int (+ 0.5 (* 60.0 (- pace (int pace)))))]
+    (str (int pace) ":" secs "min/km")))
+
 (defn pace [db-retmap]
-   (str (/ (/ (get db-retmap :tdur) 6000) (/ (get db-retmap :tdist) 1000))))
+  (/ (/ (get db-retmap :tdur) 6000) (/ (get db-retmap :tdist) 1000)))
 
 (defn to-summary [db-object]
   (let [db-retmap (first (get db-object :retval))
         count (int (get db-object :count))]
     {:duration (to-human-time (int (get db-retmap :dur)))
      :distance (to-human-distance (int (get db-retmap :dist)))
-     :pace (pace db-retmap)
+     :pace (to-human-pace (pace db-retmap))
+     :avghr (/ (get db-retmap :tavghr) (get db-retmap :hrcount))
      :count count}))
-               
-(defn get-db-summary [condition]
-  (from-db-object (command {:group {:ns "exercise"
-                                    :cond condition 
-                                    :$reduce js-reduce
-                                    :initial {:dist 0 :dur 0 :tdist 0 :tdur 0}}})
-                  true))
-  
-;; Example of db.group -command
+
+;; Example of db.group -command in mongodb format.
 ;; db.exercise.group({cond: {user: "username"}, reduce: function(obj, prev) { prev.csum += obj.distance }, initial: {csum: 0 }});
-;; condition e.g. :user user
+(defn get-db-summary [condition]
+    ;; Pace computation uses exercises, where both distance and duration are known. Those sums are recorded to tdur and tdist
+    ;; while reducing, and dist and dur are plain sums over the db values.
+  (let [js-reduce "function(exercise, prev) {
+                     if (exercise.distance > 0 && exercise.duration > 0) {
+                        prev.tdist += exercise.distance;
+                        prev.tdur += exercise.duration;
+                      }
+                      if (exercise.avghr > 0) {
+                        prev.tavghr += exercise.avghr;
+                        prev.hrcount += 1;  
+                      }
+                      prev.dist += exercise.distance;
+                      prev.dur += exercise.duration }"]
+    (from-db-object (command {:group {:ns "exercise"
+                                      :cond condition 
+                                      :$reduce js-reduce
+                                      :initial {:tavghr 0
+                                                :hrcount 0
+                                                :dist 0
+                                                :dur 0
+                                                :tdist 0
+                                                :tdur 0}}})
+                    true)))
+  
 (defn get-summary [condition]
   (to-summary (get-db-summary condition)))
 
+(defn get-year-summary [user year]
+  (let [first-day (time/date-time year 1 1)
+        last-day (time/date-time year 12 31)]
+    (get-summary {:user user :date {:$gte first-day :$lte last-day}})))
+
+(defn get-year-summary-sport [user year sport]
+  (let [first-day (time/date-time year 1 1)
+        last-day (time/date-time year 12 31)]
+    (get-summary {:sport sport :user user :date {:$gte first-day :$lte last-day}})))
