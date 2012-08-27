6106ab6a04c2ba783e50d311c0e688e9245dc939
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-08-27 20:07:13 +0300

    Add utility functions

---
+++ src/main/webapp/js/rx.ontrail.js
@@ -0,0 +1,40 @@
+(function() {
+
+  Rx.Observable.prototype.selectAjax = function(ajaxAction) {
+    var stream = this.select(ajaxAction).switchLatest().publish()
+    stream.connect()
+    return stream
+  }
+
+  Rx.Observable.prototype.mergeErrors = function() {
+    var merged = new Rx.Subject()
+    this.doAction(merged.onNext, merged.onNext, merged.onCompleted)
+      .retry().publish().connect()
+    return merged
+  }
+
+  Rx.Observable.prototype.mergeTo = function(observable) {
+    return Rx.Observable.merge(null, this, observable)
+  }
+  Rx.Observable.prototype.combineWithLatestOf = function(second, combinator) {
+    var first = this
+    var latest
+    second.subscribe(function(value) { latest = value })
+
+    return Rx.Observable.create(function(subscriber) {
+      var seq = first.subscribe(function(value) {
+        subscriber.onNext(combinator(value, latest))
+      })
+      return function() { seq.dispose() }
+    })
+  }
+}());
+
+var debug = function() {console.log("debug: ", arguments)}
+var always = function(x) { return function(y) { return x } }
+var identity = function(x) { return x }
+var not = function(x) { return !x }
+var first = function() { return arguments[0] }
+var second = function() { return arguments[1] }
+
+
