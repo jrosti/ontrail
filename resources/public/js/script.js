f73a349876d57a274a6c15ac45ae00ad0eea0d70
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-08-27 21:35:18 +0300

    Render summary data

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,16 +1,24 @@
 
 (function() {
+  $(document).ready(function() {
+    var source = $("#summary-entry-template").html();
+    var entryTemplate = Handlebars.compile(source);
 
-  var getSummary = function(user) {
-    return $.ajaxAsObservable({ url: "http://localhost:8080/summary/" + user })
-  }
+    var getSummary = function(user) {
+      return $.ajaxAsObservable({ url: "http://localhost:8080/summary/" + user })
+    }
 
-  var drawSummary = function(data) {
-    debug(data)
-  }
+    var drawSummary = function(data) {
+      var content = _.map(data, entryTemplate).reduce(function(a, b) { return a+b })
+      debug(data, content)
+      $('#summary-entries').html(content)
 
-  var summaryRequests = $('#user').changeAsObservable().select(function(event) { return event.target.value }).selectAjax(getSummary)
-  summaryRequests.where(isSuccess).select(ajaxResponseData).subscribe(drawSummary)
+    }
+
+    var summaryRequests = $('#user').changeAsObservable().select(function(event) { return event.target.value }).selectAjax(getSummary)
+    summaryRequests.where(isSuccess).select(ajaxResponseData).subscribe(drawSummary)
+
+  })
 
 })()
 
