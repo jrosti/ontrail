1280944453be1cb0d91084c03aa253ce24a7c6fe
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-08-27 20:41:25 +0300

    get updated summary whenever username is changed.

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,6 +1,17 @@
 
 (function() {
-  $('#user').changeAsObservable().select(function(event) { return event.target.value }).subscribe(function(data) { console.log("hello world", data) })
+
+  var getSummary = function(user) {
+    return $.ajaxAsObservable({ url: "http://localhost:8080/summary/" + user })
+  }
+
+  var drawSummary = function(data) {
+    debug(data)
+  }
+
+  var summaryRequests = $('#user').changeAsObservable().select(function(event) { return event.target.value }).selectAjax(getSummary)
+  summaryRequests.where(isSuccess).select(ajaxResponseData).subscribe(drawSummary)
+
 })()
 
 
