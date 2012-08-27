1a4aacb828ce9207f37fcbaaf38868d829ac3e47
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-08-27 20:07:35 +0300

    RxJS test

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,8 +1,6 @@
 
 (function() {
-
-  
-
+  $('#user').changeAsObservable().select(function(event) { return event.target.value }).subscribe(function(data) { console.log("hello world", data) })
 })()
 
 
