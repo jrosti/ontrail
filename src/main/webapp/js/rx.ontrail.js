6b01fa6e6deb8a274c6fc318cec93d28a4b11b29
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-08-27 20:40:49 +0300

    utility functions for mapping and filtering http responses

--- src/main/webapp/js/rx.ontrail.js
+++ src/main/webapp/js/rx.ontrail.js
@@ -31,10 +31,13 @@
 }());
 
 var debug = function() {console.log("debug: ", arguments)}
+
 var always = function(x) { return function(y) { return x } }
 var identity = function(x) { return x }
 var not = function(x) { return !x }
 var first = function() { return arguments[0] }
 var second = function() { return arguments[1] }
 
+var isSuccess = function(response) { return response.jqXHR.status >= 200 && response.jqXHR.status < 400 }
+var ajaxResponseData = function(response) { return response.data }
 
