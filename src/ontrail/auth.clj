cc3ba14a919e421282951995e2b265c986ddaa65
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-08-28 23:16:15 +0300

    Move functions to crypto namespace

--- src/ontrail/auth.clj
+++ src/ontrail/auth.clj
@@ -1,12 +1,6 @@
 (ns ontrail.auth
   (:use ontrail.user ontrail.crypto))
 
-(import com.lambdaworks.codec.Base64)
-
-(defn byte-array-to-string [arr] (apply str arr))
-(defn base64-encode [string]
-  (byte-array-to-string (Base64/encode (.getBytes string))))
-
 (defn authenticate [user password]
   (= (password-match password (:passwordHash (get-user user)))))
 
--- src/ontrail/crypto.clj
+++ src/ontrail/crypto.clj
@@ -1,5 +1,11 @@
 (ns ontrail.crypto)
 
+(import com.lambdaworks.codec.Base64)
+
+(defn byte-array-to-string [arr] (apply str arr))
+(defn base64-encode [string]
+  (byte-array-to-string (Base64/encode (.getBytes string))))
+
 (defn password-hash [password]
   (com.lambdaworks.crypto.SCryptUtil/scrypt password 16384 8 1))
 
