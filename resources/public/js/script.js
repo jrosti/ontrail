99776c87588360b2b5fc32daf8b006698fb7204d
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-08-28 07:22:15 +0300

    Implement a real login form and bind fetching data to it.

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -8,14 +8,22 @@
       return $.ajaxAsObservable({ url: "http://localhost:8080/summary/" + user })
     }
 
+    var doLogin = function() {
+      console.log($('#login-form').serialize())
+      return $.ajaxAsObservable({ type: 'POST', url: "http://localhost:8080/login", data: $('#login-form').serialize() })
+    }
+
+
     var drawSummary = function(data) {
       var content = _.map(data, entryTemplate).reduce(function(a, b) { return a+b })
       debug(data, content)
       $('#summary-entries').html(content)
-
     }
 
-    var summaryRequests = $('#user').changeAsObservable().select(function(event) { return event.target.value }).selectAjax(getSummary)
+    var logins = $("#login").clickAsObservable().selectAjax(doLogin)
+    logins.subscribe(debug)
+
+    var summaryRequests = logins.where(isSuccess).select(ajaxResponseData).select(function(data) { return data.user }).selectAjax(getSummary)
     summaryRequests.where(isSuccess).select(ajaxResponseData).subscribe(drawSummary)
 
   })
