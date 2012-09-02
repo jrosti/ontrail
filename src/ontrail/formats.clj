7216f49a024740889e556571d55cc67014e687c8
Jari Rosti <jari.rosti@syslore.com>
2012-09-02 21:31:51 +0300

    tinkering km/h for pyöräily and min/km for other sports

--- src/ontrail/formats.clj
+++ src/ontrail/formats.clj
@@ -11,16 +11,27 @@
 (defn positive-numbers? [vals]
   (reduce #(and %1 %2) (map #(and (not-nil? %) (> % 0)) vals)))
 
-(defn to-human-pace [pace]
-  (let [secs (int (+ 0.49 (* 60.0 (- pace (int pace)))))]
+(defn to-human-pace-minkm [duration distance]
+  (let [pace  (/ (/ duration 6000) (/ distance 1000))
+        secs (int (+ 0.49 (* 60.0 (- pace (int pace)))))]
     (str (int pace) ":" (format "%02d" secs) "min/km")))
+
+(defn to-human-pace-kmh [duration distance]
+  (let [speed (/ (/ distance 1000.0) (/ duration 360000.0))
+        fraction (int (* 100 (- speed (int speed))))]
+    (str (int speed) "." (format "%02d" fraction) "km/h")))
+
+(defn pace-conversion-fun [sport]
+  (case sport
+    "Pyöräily" to-human-pace-kmh
+    to-human-pace-minkm))
 
 (defn get-pace [exercise]
   (let [duration (get exercise :duration)
-        distance (get exercise :distance)]
+        distance (get exercise :distance)
+        pace-fun (pace-conversion-fun (get exercise :sport))]
     (if (positive-numbers? (list duration distance))
-      (to-human-pace (/ (/ duration 6000) (/ distance 1000)))
-      "")))
+      (pace-fun duration distance))))
 
 (defn to-human-distance [distance]
   (let [km (int (/ distance 1000))
--- test/ontrail/test/formats.clj
+++ test/ontrail/test/formats.clj
@@ -4,12 +4,16 @@
 
 (require '[clj-time.core :as time])
 
-(deftest human-pace
-  (is (= "6:00min/km" (to-human-pace 6.0)) "Basic pace test."))
+(deftest test-to-human-pace-minkm
+  (is (= "6:00min/km" (to-human-pace-minkm (* 100 60 6) 1000)) "Basic pace test."))
 
-(deftest pace-from-exercise
+(deftest test-pace-from-exercise
   (let [ex {:duration 30000 :distance 1000}] ;; is 5min/km
     (is (= "5:00min/km" (get-pace ex)) "Pace in min/km.")))
 
-(deftest user-time-format
+(deftest test-kmh-pace-from-exercise
+  (let [ex {:duration (* 210 100 60) :distance 42195 :sport "Pyöräily"}]
+    (is (= "12.05km/h" (get-pace ex)) "Driving marathon 3:30 time.")))
+
+(deftest test-user-time-format
   (is (= "11.05.1978" (to-human-date (time/date-time 1978 5 11))) "Default formatting for exercise time"))
