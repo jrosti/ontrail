76fc2cca44d60a662062a5e8adb0d98f5fdcaec7
Jari Rosti <jari.rosti@syslore.com>
2012-09-09 16:15:09 +0300

    removing test users correctly

--- remove-esko.js
+++ remove-esko.js
@@ -1,4 +1,4 @@
 db.onuser.remove({username: "Esko"});
 db.onuser.remove({username: "Urpo"});
-db.exercise.remove({username: "Esko"});
-db.exercise.remove({username: "Urpo"});
+db.exercise.remove({user: "Esko"});
+db.exercise.remove({user: "Urpo"});
