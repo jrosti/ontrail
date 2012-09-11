91f1188cee4811b759ca44a61c72cdba70eaa8ec
Jari Rosti <jari.rosti@syslore.com>
2012-09-11 17:59:56 +0300

    cleanrun creates real user

--- remove-esko.js
+++ remove-esko.js
@@ -1,4 +1,4 @@
-db.onuser.remove({username: "Esko"});
+db.onuser.remove({username: "Peppi"});
 db.onuser.remove({username: "Urpo"});
-db.exercise.remove({user: "Esko"});
+db.exercise.remove({user: "Peppi"});
 db.exercise.remove({user: "Urpo"});
