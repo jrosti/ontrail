d112bfc81abd300ef6c71240f458de703cf83ba5
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-09-11 11:55:34 +0300

    Utilities updated.
    asArgs and selectArgs are to be used to unwrap array responses to function call arguments

--- resources/public/js/rx.ontrail.js
+++ resources/public/js/rx.ontrail.js
@@ -1,7 +1,13 @@
 (function() {
 
+  Rx.Observable.prototype.selectArgs = function(selectAction) {
+    return this.select(function(args) {
+      return selectAction.apply(null, asArgs(args))
+    })
+  }
+
   Rx.Observable.prototype.selectAjax = function(ajaxAction) {
-    var stream = this.select(ajaxAction).switchLatest().publish()
+    var stream = this.selectArgs(ajaxAction).switchLatest().publish()
     stream.connect()
     return stream
   }
@@ -34,12 +40,17 @@
 var debug = function() {console.log("debug: ", arguments)}
 var nothing = function() {}
 
+// general utilities
+var splitWith = function(delim, string) { return string.split(delim) }
+
 // functional js
+var asArgs = function() { return $.isArray(arguments[0]) ? arguments[0] : arguments }
+
 var always = function(x) { return function(y) { return x } }
 var identity = function(x) { return x }
 var not = function(x) { return !x }
-var first = function() { return arguments[0] }
-var second = function() { return arguments[1] }
+var first = function() { return asArgs.apply(null, arguments)[0] }
+var second = function() { return asArgs.apply(null, arguments)[1] }
 var equals = function( a, b ) { return a === b }
 var partialEquals = function(val) { return _.partial(equals, val) }
 
@@ -51,6 +62,7 @@ var ajaxResponseData = function(response) { return response.data }
 var eventTarget = function(event) { return event.target }
 
 // DOM
+var attr = function(name, el) { return $(el).attr(name) }
 var elementBottomIsAlmostVisible = function(el, margin) {
   // Toim.huom. $(window).height() näyttäis olevan rikki jquery 1.8.1:ssä...
   var viewportBottom = $(window).scrollTop() + window.innerHeight
