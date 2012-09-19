119f890750a027bf2ba08ad12fd0859ca67bcab2
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-09-19 23:04:59 +0300

    fix session handling for logouts.

--- resources/public/js/rx.ontrail.session.js
+++ resources/public/js/rx.ontrail.session.js
@@ -1,10 +1,10 @@
 
 (function(){
   var Session = function() {}
-  Session.prototype.create = function(logins, loginFails, logouts) {
+  Session.prototype.create = function(logins, logouts) {
     return rx.create(function(observer) {
       logins.subscribe(function(login) { $.cookie("authToken", login.token ); $.cookie("authUser", login.username); observer.onNext(login.username) } )
-      logouts.mergeTo(loginFails).subscribe(function() { $.cookie("authToken", null); $.cookie("authUser", null); observer.onNext(null)})
+      logouts.subscribe(function() { $.cookie("authToken", null); $.cookie("authUser", null); observer.onNext(null)})
       observer.onNext($.cookie("authUser")) // initialize with login state
       return function() {} // todo -- should we dispose something.
     })
