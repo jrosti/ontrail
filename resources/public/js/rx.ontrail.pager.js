fdd274481582d950d5da3deca329e3c4a105a52b
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-09-19 23:04:47 +0300

    fix pager -> empty is better than never for ending streams.

--- resources/public/js/rx.ontrail.pager.js
+++ resources/public/js/rx.ontrail.pager.js
@@ -4,7 +4,7 @@
   var pager = function(ajaxSearch, page, next) {
     return ajaxSearch(page).selectMany(function(res) {
       if (res.length === 0)
-        return rx.never()
+        return rx.empty()
       else
         return rx.returnValue(res).concat(next.take(1).selectMany(function() { return pager(ajaxSearch, page+1, next) }))
     })
