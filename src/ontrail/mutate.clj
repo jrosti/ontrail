1de06791e4434960c2736af6bf0d15a086b7adab
Jari Rosti <jari.rosti@syslore.com>
2012-09-25 22:45:34 +0300

    simplifications to re-rule based parsing

--- src/ontrail/mutate.clj
+++ src/ontrail/mutate.clj
@@ -47,8 +47,8 @@
     {:re #"^([0-9]+)[:\.]([0-9]+)[:\.]([0-9]+)$" :conv hours-and-minutes-and-seconds}
     {:re #"^([0-9]+) min ([0-9]+),([0-9]) s$" :conv minutes-and-seconds-and-tenths}
     {:re #"^([0-9]+)\.([0-9]+),([0-9])$" :conv minutes-and-seconds-and-tenths}
-    {:re #"^([0-9]+)\.([0-9]+),([0-9][0-9])$" :conv minutes-and-seconds-and-tenths-and-hundreds}
-    {:re #"^([0-9]+) min ([0-9]+),([0-9])([0-9]) s$" :conv minutes-and-seconds-and-tenths-and-hundreds}])
+    {:re #"^([0-9]+)\.([0-9]+),([0-9][0-9])[^0-9]$" :conv minutes-and-seconds-and-tenths-and-hundreds}
+    ])
 
 (defn try-match [re str]
   (re-matches re str))
