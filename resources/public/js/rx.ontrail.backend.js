dcad606a462e5317925810e308fb59c0645a7308
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-09-25 18:56:21 +0300

    getAsObservable separated from success result

--- resources/public/js/rx.ontrail.backend.js
+++ resources/public/js/rx.ontrail.backend.js
@@ -6,20 +6,27 @@
     errorHandler = { error: function(error) { observer.onNext({ jqXHR: error }) } }
     return function() { errorHandler = null }
   });
+  var success = function(ajaxStream) {
+    return ajaxStream.where(isSuccess).select(ajaxResponseData)
+  }
 
   var getAsObservable = function() {
     var path = _.reduce(arguments, function(a, b) { return a + "/" + b })
-    return $.ajaxAsObservable($.extend({ url: "/rest/v1/" + path }, errorHandler)).mergeTo(ajaxErrors).where(isSuccess).select(ajaxResponseData)
+    return $.ajaxAsObservable($.extend({ url: "/rest/v1/" + path }, errorHandler)).mergeTo(ajaxErrors)
   }
-  Rest.prototype.summary = function(user) { return getAsObservable("summary", user) }
+  var getAsObservableResultData = function() {
+    return success(getAsObservable.apply(this, arguments))
+  }
+
+  Rest.prototype.summary = function(user) { return getAsObservableResultData("summary", user) }
   // unused Jro
-  Rest.prototype.avatarUrl = function(user) { return getAsObservable("avatar", user) }
-  Rest.prototype.latest = function(page) { return getAsObservable("ex-list-all", page) }
-  Rest.prototype.userExercises = function(user, page) { return getAsObservable("ex-list-user", user, page) }
-  Rest.prototype.tagExercises = function(tag, page) { return getAsObservable("ex-list-tag", tag, page) }
-  Rest.prototype.details = function(kind, id) { return getAsObservable(kind, id) }
-  Rest.prototype.searchResults = function(query) { return getAsObservable("search?q=" + query ) }
-  Rest.prototype.sports = function() { return getAsObservable("sports") }
+  Rest.prototype.avatarUrl = function(user) { return getAsObservableResultData("avatar", user) }
+  Rest.prototype.latest = function(page) { return getAsObservableResultData("ex-list-all", page) }
+  Rest.prototype.userExercises = function(user, page) { return getAsObservableResultData("ex-list-user", user, page) }
+  Rest.prototype.tagExercises = function(tag, page) { return getAsObservableResultData("ex-list-tag", tag, page) }
+  Rest.prototype.details = function(kind, id) { return getAsObservableResultData(kind, id) }
+  Rest.prototype.searchResults = function(query) { return getAsObservableResultData("search?q=" + query ) }
+  Rest.prototype.sports = function() { return getAsObservableResultData("sports") }
 
   // todo: move login and postExercise here also and make postAsObservable private
   Rest.prototype.postAsObservable = function(url, data) {
