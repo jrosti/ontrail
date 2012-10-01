8a939141e9de25819fb50a86c169249f5288a605
Jari Rosti <jari.rosti@syslore.com>
2012-10-01 19:07:51 +0300

    refactored mutate to parser

--- src/ontrail/mutate.clj
+++ src/ontrail/mutate.clj
@@ -1,95 +1,15 @@
 (ns ontrail.mutate
-  (:use [ontrail mongodb utils search exercise user formats])
+  (:use [ontrail mongodb search exercise user formats parser])
   (:require [monger.collection :as mc]
             [monger.result :as mr]
             [monger.query :as mq]
             [monger.conversion]
-            [clj-time.format :as format]
-            [clojure.string :as string]
             [clj-time.core :as time]
             ;; for date serialization to mongo.
             [monger.joda-time])
   (:import [org.bson.types ObjectId]))
 
 (def #^{:private true} logger (org.slf4j.LoggerFactory/getLogger (str *ns*)))
-
-(def multi-parser (format/formatter (time/default-time-zone)
-                                    "dd.MM.yyyy"
-                                    "dd-MM-yyyy"
-                                    "dd/MM/yyyy"
-                                    "yyyy/MM/dd"))
-
-
-(defn as-number [x] (Integer. x))
-(defn to-db [min] (* (Integer. min) 60 100))
-(defn minutes [min] (to-db min))
-(defn hours [h] (* (to-db h) 60))
-(defn secs [sec] (* (Integer. sec) 100))
-(defn hours-and-minutes [h min] (+ (hours h) (to-db min)))
-(defn minutes-and-seconds [min sec] (+ (minutes min) (secs sec)))
-(defn hours-and-minutes-and-seconds [h min sec] (+ (hours h) (minutes min) (secs sec)))
-(defn minutes-and-seconds-and-tenths [min sec tenths]  
-  (+ (to-db min) (secs sec) (* 10 (Integer. tenths))))
-(defn minutes-and-seconds-and-tenths-and-hundreds [min sec tenths hundreds]
-  (+ (to-db min) (secs sec) (* 10 (Integer. tenths)) (Integer. hundreds)))
-
-(defn kilometers [km] (* (Integer. km) 1000))
-(defn kilometers-and-meters [km m] (+ (as-number m) (kilometers km)))
-
-(def duration-regexps
-  [ {:re #"^([0-9]+) *m[^0-9]*$" :conv minutes}
-    {:re #"^([0-9]+)$" :conv minutes}
-    {:re #"^([0-9]+).*h$" :conv hours}
-    {:re #"^([0-9]+).*h[^0-9]*([0-9]+)[^0-9]*$" :conv hours-and-minutes}
-    {:re #"^([0-9]+)\.([0-9]+)[^0-9]*$" :conv minutes-and-seconds}
-    {:re #"^([0-9]+)[^0-9]*min[^0-9]*([0-9]+)[^0-9]*s[^0-9]*$" :conv minutes-and-seconds}
-    {:re #"^([0-9]+):([0-9]+)$" :conv hours-and-minutes}
-    {:re #"^([0-9]+) h ([0-9]+) min$" :conv hours-and-minutes}
-    {:re #"^([0-9]+) h ([0-9]+) min ([0-9]+) s$" :conv hours-and-minutes-and-seconds}
-    {:re #"^([0-9]+)[:\.]([0-9]+)[:\.]([0-9]+)$" :conv hours-and-minutes-and-seconds}
-    {:re #"^([0-9]+) min ([0-9]+),([0-9]) s$" :conv minutes-and-seconds-and-tenths}
-    {:re #"^([0-9]+)\.([0-9]+),([0-9])[^0-9]*$" :conv minutes-and-seconds-and-tenths}
-    {:re #"^([0-9]+)\.([0-9]+),([0-9])([0-9])[^0-9]*$" :conv minutes-and-seconds-and-tenths-and-hundreds}
-    ])
-
-(defn try-match [re str]
-  (re-matches re str))
-
-(defn try-parse [re-conv str]
-  (let [match (try-match (:re re-conv) (string/trim str))]
-    (if match (apply (:conv re-conv) (rest match)))))
-
-(defn parse-duration [dur-str] 
-  (some identity (map #(try-parse % dur-str) duration-regexps)))
-
-(defn parse-date [date-str]
-  (time/plus (format/parse multi-parser date-str) (time/hours 12)))
-
-(defn as-double-km [str]
-  (let [x (string/replace str #"," ".")]
-    (int (* 1000 (Double. x)))))
-
-(def distance-regexps
-  [{:re #"^([0-9]+)$" :conv as-double-km}
-   {:re #"^([0-9]+[\.,][0-9]+) *k*m*$" :conv as-double-km}
-   {:re #"^([0-9]+) *m *$" :conv as-number}
-   {:re #"^([0-9]+) *k$" :conv kilometers}
-   {:re #"^([0-9]+) *km$" :conv kilometers}])
-
-(defn parse-distance [dist]
-  (some identity (map #(try-parse % dist) distance-regexps)))
-
-(defn parse-tags
-  ([] '())
-  ([tags]
-     (let [trim-tag #(-> % string/trim)]
-       (if (string? tags)
-         (string/split (trim-tag tags) #",")
-         '()))))
-
-(defn parse-natural [hr]
-  (if-not (= nil hr)
-    (try-parse {:re #"^([0-9]+)$" :conv as-number} hr)))
 
 (defn from-user-ex [user user-ex]
   (let [bare-ex {:title (:title user-ex)
--- test/ontrail/test/mutate.clj
+++ test/ontrail/test/mutate.clj
@@ -2,11 +2,6 @@
   (:use ontrail.mutate)
   (:use clojure.test))
 
-(deftest test-parse-tags
-  (is (= ["tag1", "tag2"] (parse-tags " tag1,tag2")))
-  (is (= ["tag1", "tag2"] (parse-tags " tag1,tag2 "))))
-
-
 (def basic-exercise {:title "A title"
                      :sport "Juoksu"
                      :duration "30"
@@ -15,18 +10,6 @@
                      :avghr "129"
                      :body "Pom"
                      :tags "tag1 tag2 tag3"})
-
-(deftest test-parse-minutes
-  (is (= (parse-duration "60") (parse-duration "60 m")))
-  (is (= (parse-duration "60") (parse-duration "60m")))
-  (is (= (parse-duration "60") (parse-duration "1 h")))
-  (is (= (hours 1) (hours-and-minutes 1 0)))
-  (is (= (parse-duration "1:0") (hours 1)))
-  (is (= (parse-duration "1.0") 6000))
-  (is (= (parse-duration "59.30") (+ (* 59 6000) 3000)))
-  (is (= (parse-duration "0.59.30") (+ (* 59 6000) 3000)))
-  (is (= (parse-duration "0:59:30") (+ (* 59 6000) 3000)))
-  (is (= (parse-duration "2.42,2") 16220)))
   
 (deftest test-db-insertable-from-basic-user-ex
   (let [uex (from-user-ex "Esko" basic-exercise)]
