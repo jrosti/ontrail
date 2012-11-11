bf5d12be476038b1360358f7aea6422264ef686f
Jari Rosti <jari.rosti@syslore.com>
2012-11-12 00:08:03 +0200

    oops, doesnt compile

--- src/ontrail/auth.clj
+++ src/ontrail/auth.clj
@@ -33,6 +33,6 @@
   (try
       (:username (user-from-token (:value (cookies "authToken"))))
       (catch Exception exception
-        (.error trace (str "Could not get user from cookie " exception))
+        (.trace logger (str "Could not get user from cookie " exception))
         "nobody")))
     
