7397662584d14988760c53fb5b6d562c1a4fc6a0
Jari Rosti <jari.rosti@syslore.com>
2012-11-11 13:08:36 +0200

    user verification and lowercase gravatar

--- src/ontrail/user.clj
+++ src/ontrail/user.clj
@@ -7,7 +7,7 @@
 
 (defn as-gravatar [user]
   (let [gravatar? (get user :gravatar)
-        email (get user :email)
+        email (.toLowerCase (get user :email))
         gravatar-md5-hash (md5 email)]
     (if gravatar?
       (str "http://www.gravatar.com/avatar/" gravatar-md5-hash)
@@ -23,6 +23,9 @@
       (as-gravatar onuser)
       "/img/drno.png")))
 
+(defn get-user [username]
+  (mc/find-one-as-map ONUSER {:username username}))
+
 (defn get-user-list [rule page]
   (let [results (mq/with-collection ONUSER
     (mq/find rule)
@@ -34,15 +37,12 @@
 (defn create-user [username password email gravatar]
   (let [profile {:resthr 42 :maxhr 192}]
     (.info logger (str "creating user " username " with profile " profile))
-    (if (and (not= username "") (not= username "nobody") (> (count (get-user-list {:username username} 1)) 0))
+    (if (and (not= username "") (not= username "nobody") (= nil (get-user username)))
       (mc/insert-and-return ONUSER {:username username :passwordHash (password-hash password) :email email :profile profile :gravatar (java.lang.Boolean. gravatar)})
       (.error logger (str "creating user failed " username " with profile " profile)))))
 
 (defn register-user [params]
   (create-user (:username params) (:password params) (:email params) true))
-
-(defn get-user [username]
-  (mc/find-one-as-map ONUSER {:username username}))
 
 (defn -main[& args]
   (let [[username password email has-gravatar & rest] args]
