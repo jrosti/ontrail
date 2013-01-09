91a9484e6f49dc3ebde637e050e2681ac576db0e
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2013-01-09 14:21:24 +0200

    clickAsObservable replaced with touch -capable onAsObservable

--- resources/public/js/ontrail.js
+++ resources/public/js/ontrail.js
@@ -151,7 +151,7 @@
       }
 
       if ($.isArray(summary)) {
-        var hasSports = function(item) { return item.sports.length > 1 }
+        var hasSports = function(item) { return item.sports.length > 0 }
         var extendWithMonthName = function(item) { return _.extend(item, monthNames ) }
         var sums = _.map(_.filter(summary, hasSports), extendWithMonthName)
         var sum = _.extend( { year: (summary[0].year) }, { months: sums, "user": summary[0].user }, utils)
@@ -191,10 +191,10 @@
 
     // logged in state handling
     var doLogin = function() { return OnTrail.rest.postAsObservable("login", $('#login-form').serialize()) }
-    var logouts = $("#logout").clickAsObservable()
+    var logouts = $("#logout").onAsObservable("click touchstart")
     var isEnter = function(event) { return event.keyCode == 13; }
     var loginEnters = $("#password").keyupAsObservable().where(isEnter)
-    var loginRequests = $("#login").clickAsObservable().merge(loginEnters).selectAjax(doLogin)
+    var loginRequests = $("#login").onAsObservable("click touchstart").merge(loginEnters).selectAjax(doLogin)
     var logins = loginRequests.where(isSuccess).select(ajaxResponseData)
     var loginFails = loginRequests.where(_.compose(not, isSuccess)).select(ajaxResponseData)
 
@@ -390,7 +390,7 @@
     }
 
     var showExercise = function(ex) { showPage("ex", ex.id); renderSingleExercise(ex) }
-    var addExercises = $('#add-exercise').clickAsObservable().select(target).where(_.compose(not, _hasClass("disabled"))).combineWithLatestOf(sessions).selectArgs(second).where(exists).selectAjax(postAddExercise).where(isSuccess).select(ajaxResponseData)
+    var addExercises = $('#add-exercise').onAsObservable("click touchstart").select(target).where(_.compose(not, _hasClass("disabled"))).combineWithLatestOf(sessions).selectArgs(second).where(exists).selectAjax(postAddExercise).where(isSuccess).select(ajaxResponseData)
     addExercises.subscribe(showExercise)
     currentPages.whereArgs(partialEquals("addex")).subscribeArgs(function(page, exid) {
       if (exid === undefined) resetEditor()
@@ -440,14 +440,14 @@
       resetEditor()
       $("[role='addex']").attr('data-mode', 'add')
     }
-    $('.pageLink[rel="addex"]').clickAsObservable().subscribe(renderAddExercise)
+    $('.pageLink[rel="addex"]').onAsObservable("click touchstart").subscribe(renderAddExercise)
 
     var asExercise = function(__, exercise) { return ["ex", exercise] }
     var editExercise = currentPages.whereArgs(function(page, subPage) { return page === "addex" && subPage })
     editExercise.selectArgs(asExercise).selectAjax(OnTrail.rest.details).subscribe(renderEditExercise)
 
     // muokkauksen submit
-    var updateExercises = $('#edit-exercise').clickAsObservable()
+    var updateExercises = $('#edit-exercise').onAsObservable("click touchstart")
       .combineWithLatestOf(editExercise).selectArgs(_.compose(second, second)).selectAjax(postEditExercise).where(isSuccess).select(ajaxResponseData)
     updateExercises.subscribe(showExercise)
 
@@ -457,7 +457,7 @@
     updateProfiles.subscribeArgs(renderProfileUpdate)
 
     // Lisää kommentti
-    var addComments = $('#exercise').clickAsObservable().select(target).where(function(el) { return el.id === "add-comment"})
+    var addComments = $('#exercise').onAsObservable("click touchstart").select(target).where(function(el) { return el.id === "add-comment"})
       .combineWithLatestOf(exPages).selectArgs(second).select(id).selectAjax(postComment).where(isSuccess).select(ajaxResponseData)
     addComments.combineWithLatestOf(sessions).subscribeArgs(renderSingleExercise)
 
