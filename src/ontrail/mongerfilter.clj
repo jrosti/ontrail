d2bc1832f93f6aae911e51b332959ab0a9a78907
jro <jari.rosti@gmail.com>
2013-04-14 16:25:47 +0300

    lt and gt to creation date

--- src/ontrail/mongerfilter.clj
+++ src/ontrail/mongerfilter.clj
@@ -58,7 +58,7 @@
 
 (defn make-query-from [params]
   (let [basic-query (make-basic-query (select-keys params [:user :tags :sport :distance :duration]))
-  		date-range-query (make-range-query parse-date (select-keys params [:lte_creationDate :gte_creationDate]))
+  		date-range-query (make-range-query parse-date (select-keys params [:lt_creationDate :gt_creationDate :lte_creationDate :gte_creationDate]))
   		long-range-query (make-range-query parse-long (select-keys params (long-cmp-keys)))
   		group-query (make-group-query (:group params))]
   	{$and (vec (concat basic-query date-range-query long-range-query group-query))}))
