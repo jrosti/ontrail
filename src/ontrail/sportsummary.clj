258f58803517d128b9a09ba4bc42f3db9de9f297
jro <jari.rosti@gmail.com>
2013-05-12 17:48:49 +0300

    memoizable exercise summaries

--- src/ontrail/sportsummary.clj
+++ src/ontrail/sportsummary.clj
@@ -8,8 +8,9 @@
 
 (def #^{:private true} logger (org.slf4j.LoggerFactory/getLogger (str *ns*)))
 
+(def summary-memos (ref {}))
+
 (defn get-overall-summary-cond [user condition]
-  (.trace logger (str "Getting summary: " user " condition " condition))  
   (let [cond-with-user (assoc condition :user user)
         all-distinct-sports (mc/distinct EXERCISE "sport" cond-with-user)
         summary-sports (sort-by :numericalDuration > (map #(get-summary (assoc cond-with-user :sport %) :sport %) all-distinct-sports))]
@@ -17,18 +18,32 @@
       {:user user :sports (concat summary-sports [(get-summary cond-with-user :sport "YHTEENSÄ")])}
       {:user user :sports summary-sports})))
 
+(defn memo-for [user]
+  (if-let [summary-memo (@summary-memos user)]
+    summary-memo
+    (let [new-memo (memoize (partial get-overall-summary-cond user))]
+      (alter summary-memos assoc user new-memo)
+      new-memo)))
+
 (defn get-year-summary-sport [user year]
-  (let [first-day (time/date-time year 1 1)
-        last-day (time/date-time year 12 31 23 59)
-        year-cond {:creationDate {:$gte first-day :$lte last-day}}]
-    (assoc (get-overall-summary-cond user year-cond) :year year)))
+  (dosync (let [first-day (time/date-time year 1 1)
+                last-day (time/date-time year 12 31 23 59)
+                year-cond {:creationDate {:$gte first-day :$lte last-day}}
+                summary-memo (memo-for user)]
+    (assoc (summary-memo year-cond) :year year))))
 
 (defn get-month-summary-sport [user year month]
-  (let [first-day (time/date-time year month 1)
-        last-day (time/plus (-> first-day (.dayOfMonth) (.withMaximumValue)) (time/hours 23))
-        year-month-cond {:creationDate {:$gte first-day :$lte last-day}}]
-    (assoc (get-overall-summary-cond user year-month-cond) :year year :month (- month 1))))
+  (dosync (let [first-day (time/date-time year month 1)
+                last-day (time/plus (-> first-day (.dayOfMonth) (.withMaximumValue)) (time/hours 23))
+                year-month-cond {:creationDate {:$gte first-day :$lte last-day}}
+                summary-memo (memo-for user)]
+    (assoc (summary-memo year-month-cond) :year year :month (- month 1)))))
 
 (defn get-overall-summary [user]
-  (.trace logger (str "Getting overall summary: " user))
-  (get-overall-summary-cond user {}))
+  (dosync (let [summary-memo (memo-for user)]
+    (summary-memo {}))))
+
+(defn reset-memo-for [user]
+  (dosync (alter summary-memos dissoc user))
+  (future get-overall-summary user)
+  nil)
