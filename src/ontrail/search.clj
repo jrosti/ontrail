a1bc655c1c7f5d58e990ca84216ebe8fe78cf99d
jro <jari.rosti@gmail.com>
2013-06-29 18:08:19 +0300

    search index to use STM, and is thread safe for modifications

--- src/ontrail/search.clj
+++ src/ontrail/search.clj
@@ -8,27 +8,24 @@
   (:import [org.bson.types ObjectId]))
 
 (def minimum-term-length 2)
-(def search-limit 100)
-(def not-too-short-term? #(>= (count %) minimum-term-length))
+(def search-per-page 20)
+(def not-too-short? #(>= (count %) minimum-term-length))
 
 (def #^{:private true} logger (org.slf4j.LoggerFactory/getLogger (str *ns*)))
 
 (defn tags-to-string [tags]
   (apply str (interpose " " tags)))
 
-(defn kms[d]
-  (str "km:" (quot d 1000)))
-
 (def re-term #"[a-zåäö#0-9\-]+")
 
 (defn to-term-seq [^String words]
   (if words
-    (re-seq re-term (.toLowerCase (strip-html words)))
+    (filter not-too-short? (re-seq re-term (.toLowerCase (strip-html words))))
     []))
 
-(defn to-bare-term-list [exercise] 
+(defn to-term-list [exercise] 
   (concat 
-   (to-term-seq (tags-to-strings (:tags exercise)))
+   (to-term-seq (tags-to-string (:tags exercise)))
    (to-term-seq (:user exercise))
    (to-term-seq (:body exercise))
    (to-term-seq (:title exercise))
@@ -41,11 +38,9 @@
                      ))
 
 (defn get-ex-terms [exercise]
-  (let [bare-term-list (to-bare-term-list exercise)
-        term-list (filter not-too-short-term? bare-term-list)]
-    term-list))      
+    (to-term-list exercise))
           
-(def inverted-index (atom {}))
+(def inverted-index (ref {}))
 
 (defn insert-term [assoc-fn ex-id index term]
   (if-let [postings (index term)]
@@ -58,18 +53,21 @@
         ex-id (str (:_id ex))] 
     (reduce (partial insert-term assoc-fn ex-id) index terms)))
 
-(def insert-exercise-inmem-index
-  (partial insert-exercise-to-index assoc @inverted-index))
+(defn insert-exercise-inmem-index [ex]
+  (dosync (alter inverted-index (partial insert-exercise-to-index assoc) ex)))
   
 (defn rebuild-index []
   (let [new-index (transient {})]
-    (count (do (reset! inverted-index (persistent! (reduce (partial insert-exercise-to-index assoc!) new-index (mc/find-maps EXERCISE {}))))))))
+    (count 
+     (dosync 
+       (ref-set inverted-index 
+               (persistent! 
+                (reduce (partial insert-exercise-to-index assoc!) new-index (mc/find-maps EXERCISE {}))))))))
 
 (defn search-ids [& terms]
-  (let [filtered-terms (filter not-too-short-term? terms)]
-    (if (> (count filtered-terms) 0)
-      (take search-limit (apply clojure.set/intersection (map #(@inverted-index %) filtered-terms)))
-      '())))
+  (if (> (count terms) 0)
+    (take search-per-page (apply clojure.set/intersection (map #(@inverted-index %) terms)))
+    '()))
 
 (defn search [& terms]
   (let [ids  (apply search-ids terms)]
@@ -77,7 +75,8 @@
     (as-ex-result-list (sort-by :creationDate time/after? (filter (partial not= nil) (map #(mc/find-one-as-map EXERCISE {:_id (ObjectId. %)}) ids))))))
 
 (defn search-wrapper [query]
-  (let [query-string (:q query)]
-    (if (nil? query-string)
-      '()
-      (apply search (re-seq re-term (.toLowerCase query-string))))))
+  (let [query-string (:q query)
+        terms (filter not-too-short? (re-seq re-term (.toLowerCase query-string)))]
+    (if (> (count terms) 0)
+      []
+      (apply search terms))))
