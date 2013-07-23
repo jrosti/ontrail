68c0c4e001c31cfb0a8606cd49103885e22478da
jro <jari.rosti@gmail.com>
2013-07-23 16:22:52 +0300

    UI improvements to filters

--- resources/public/js/ontrail.js
+++ resources/public/js/ontrail.js
@@ -797,7 +797,7 @@
 
     // A static query using bit heavy mongodb offline query to suggest currently active users. 
     // Must have a cached version of this in the server side. Chzn does not limit options, and
-    // thus we can drop users
+    // thus this affects only to the autocomplete feature of selecting users.
     var activeUsers = ["-James-","20660","Anttu","BirdiBlu","Duckbill","Elwood","Emo","Epunäiti","Esteri","Ewanator","Fransa","Geoeläin","Haapis","Hazel","Heidi","Hejkki","Hietsu","HiiNokka","Holle","Hopo","Hähi","Imatran Voima","Jagge","Jiihoo","JohannaLP","Jokiv","Jukkis","Justiina_","Juupe","Jörö","KapteeniSolisluu","Kerttu","Keura","Koskaanenjuokse","Lanttu","Larry","LauraIsabella","Leena51","Lynx","MariP","Massa-Matti","Nandi","Niina","Osku","Pantse","Pasi_P","Peksu","Peppi","Pohjan Tähti","Pumppi60","Päivi","SannaK","Sehnsucht","Silu","Sirpakka","Sissi von Vuorenpeikko","Soironen","Sope","Suski","TaijaO","Tapsajussi","Tasku67","Tatteus","Tero","Tiiti54","Triina","Tuomas","Turri","Ursa Minor","Vilivilperi","admin","anatooli","arddy","berniboy","ejex","erz","ese","hannikainen","herba63","isi","jamaatta","jamo57","jarmila","jatossu","jennirinne","jogo3000","jyri","kalervo1","kalman","kata","kettis","kirva","kriish","maja","mala","mana","meusi","miguel horsehead","miklai","muhola","mummi","niilos mc","oikakati","oskuman","ousi","pellervo","peta","pietro","plouh","poko","rampako","rauman","ritaatti","rote","saarja","saavape","sakke 2","sammatti","sanina","sava","ski","tatteus","tiinu","tuomasnu","wicca"]
 
     var renderFilterValues = function() {
