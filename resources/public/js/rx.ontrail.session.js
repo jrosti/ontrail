17c56e55ca869419c34e74393a0528892ba0f559
jro <jari.rosti@gmail.com>
2013-10-27 10:22:30 +0200

    cookie expiration time

--- resources/public/js/rx.ontrail.session.js
+++ resources/public/js/rx.ontrail.session.js
@@ -3,7 +3,7 @@
   var Session = function() {}
   Session.prototype.create = function(logins, logouts) {
     return rx.create(function(observer) {
-      logins.subscribe(function(login) { $.cookie("authToken", login.token ); $.cookie("authUser", login.username); observer.onNext(login.username) } )
+      logins.subscribe(function(login) { $.cookie("authToken", login.token, { expires: 365 }  ); $.cookie("authUser", login.username, { expires: 365 } ); observer.onNext(login.username) } )
       logouts.subscribe(function() { $.cookie("authToken", null); $.cookie("authUser", null); observer.onNext(null)})
       observer.onNext($.cookie("authUser")) // initialize with login state
       return function() {} // todo -- should we dispose something.
