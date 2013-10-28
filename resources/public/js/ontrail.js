1c12a2b876a9d09fe06cc5ac739d0604efadc20a
jro <jari.rosti@gmail.com>
2013-10-29 00:17:00 +0200

    marrasputki initial stuff

--- resources/public/js/ontrail.js
+++ resources/public/js/ontrail.js
@@ -497,20 +497,8 @@
       .takeUntil(currentPages.whereArgs(_.compose(not, partialEqualsAny(["user", "tags", "sport", "group"])))).repeat()
       .subscribe(renderLatest("#content-entries", "#table-entries"))
 
-    var recordToggeler = function() {
-      $("#recordsDiv").hide()
-      $("#toggleRecords").toggle(function () {
-        $("#toggleRecords").text("Piilota juoksuennätykset")
-        $("#recordsDiv").show()
-      }, function () {
-        $("#toggleRecords").text("Näytä juoksuennätykset")
-        $("#recordsDiv").hide()
-      })
-    }
-
     var renderPageDetail = function (args) {
       $('#content-header').html("")
-
       if (args.data.action == "group" && args.data.target == "Marrasputki") {
         ich.marrasputkiTemplate(args.data).appendTo($('#content-header'))
       } else if (args.data.action == "group" && args.data.target == "RunnersHigh") {
@@ -519,9 +507,16 @@
         ich.groupDetailTemplate(args.data).appendTo($('#content-header'))
       } else if (args.data.action == "user") {
         ich.userDetailTemplate(args.data).appendTo($('#content-header'))
+        $("#recordsDiv").hide()
+        $("#toggleRecords").toggle(function () {
+          $("#toggleRecords").text("Piilota juoksuennätykset")
+          $("#recordsDiv").show()
+        }, function () {
+          $("#toggleRecords").text("Näytä juoksuennätykset")
+          $("#recordsDiv").hide()
+        })
       } else {
         ich.otherDetailTemplate(args.data).appendTo($('#content-header'))
-        recordToggeler()
       }
     }
 
