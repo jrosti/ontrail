f689bc4e3def8edd1decbb31ad351ed15cf5ebed
jro <jari.rosti@gmail.com>
2013-11-17 23:10:52 +0200

    who indented this?

--- src/ontrail/mongerfilter.clj
+++ src/ontrail/mongerfilter.clj
@@ -1,9 +1,9 @@
 (ns ontrail.mongerfilter
-	(:use [monger.operators])
-	(:require 	[clj-time.format :as format]
-				[ontrail.group :as group]
-				[clojure.string :as string]
-			  	[clj-time.core :as time]))
+  (:use [monger.operators])
+  (:require 	[clj-time.format :as format]
+                [ontrail.group :as group]
+                [clojure.string :as string]
+                [clj-time.core :as time]))
 
 (def #^{:private true} logger (org.slf4j.LoggerFactory/getLogger (str *ns*)))
 
@@ -15,58 +15,58 @@
 (defn parse-date [val] (format/parse multi-parser val))
 
 (defn parse-long [val]
-    (try
-      (Long/valueOf val)
+  (try
+    (Long/valueOf val)
     (catch Exception exception
       (.info logger (str exception " with " val))
       0)))
 
 (defn conv-fun [sport-key]
-	(if (sport-key #{:distance :duration})
-		(fn [val] (Long/valueOf val))
-		identity))
+  (if (sport-key #{:distance :duration})
+    (fn [val] (Long/valueOf val))
+    identity))
 
 (defn or-filter [sport-key catenated-value]
-	(let [convert-value (conv-fun sport-key)
-		  vals (string/split catenated-value #",")
-		  query-fun (fn [val] {sport-key (convert-value val)})]
-		(if (> (count vals) 1)
-			{$or (vec (map query-fun vals))}
-			(query-fun (first vals)))))
+  (let [convert-value (conv-fun sport-key)
+        vals (string/split catenated-value #",")
+        query-fun (fn [val] {sport-key (convert-value val)})]
+    (if (> (count vals) 1)
+      {$or (vec (map query-fun vals))}
+      (query-fun (first vals)))))
 
 (defn to-monger-range [f k v]
-	(let [op (string/split (name k) #"_")]
-		{(keyword (second op))
-			{(str "$" (first op)) (f v)}}))
+  (let [op (string/split (name k) #"_")]
+    {(keyword (second op))
+     {(str "$" (first op)) (f v)}}))
 
 (defn make-range-query [value-converter params]
-	(if (> (count params) 0)
-		(vec (map (partial apply (partial to-monger-range value-converter)) params))
-		[]))
+  (if (> (count params) 0)
+    (vec (map (partial apply (partial to-monger-range value-converter)) params))
+    []))
 
 (defn make-basic-query [query-keys]
-	(vec (map (partial apply or-filter) query-keys)))
+  (vec (map (partial apply or-filter) query-keys)))
 
 (defn make-group-query [group-name]
-	(if-let [group (group/find-by-name group-name)]
-		[{$or (vec (map (fn [user] {:user user}) (:users group)))}]
-		[]))
+  (if-let [group (group/find-by-name group-name)]
+    [{$or (vec (map (fn [user] {:user user}) (:users group)))}]
+    []))
 
 (defn long-cmp-keys[]
-	(vec (for [op ["lte" "gte" "lt" "gt"] qkey ["pace" "distance" "duration" "avghr"]]
-		(keyword (str op "_" qkey)))))
+  (vec (for [op ["lte" "gte" "lt" "gt"] qkey ["pace" "distance" "duration" "avghr"]]
+         (keyword (str op "_" qkey)))))
 
 (defn make-query-from [params]
   (let [basic-query (make-basic-query (select-keys params [:user :tags :sport :distance :duration]))
-  		date-range-query (make-range-query parse-date (select-keys params [:lt_creationDate :gt_creationDate :lte_creationDate :gte_creationDate]))
-  		long-range-query (make-range-query parse-long (select-keys params (long-cmp-keys)))
-  		group-query (make-group-query (:group params))
-  		and-query (vec (concat basic-query date-range-query long-range-query group-query))]
-  	(if (> (count and-query) 0)
-  		{$and and-query}
-  		{})))
+        date-range-query (make-range-query parse-date (select-keys params [:lt_creationDate :gt_creationDate :lte_creationDate :gte_creationDate]))
+        long-range-query (make-range-query parse-long (select-keys params (long-cmp-keys)))
+        group-query (make-group-query (:group params))
+        and-query (vec (concat basic-query date-range-query long-range-query group-query))]
+    (if (> (count and-query) 0)
+      {$and and-query}
+      {})))
 
 (defn sortby [params]
-	(if-let [sortkey (:sb params)]
-		(merge {:lastModifiedDate -1} {sortkey -1})
-		{:creationDate -1 :lastModifiedDate -1}))
+  (if-let [sortkey (:sb params)]
+    (merge {:lastModifiedDate -1} {sortkey -1})
+    {:creationDate -1 :lastModifiedDate -1}))
