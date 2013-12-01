3ff9ab879fe0bc0359cc97719d1b46fbbb07f8c6
jro <jari.rosti@gmail.com>
2013-12-01 08:50:01 +0200

    putki hall of fame to static

--- clonefun.sh
+++ clonefun.sh
@@ -2,6 +2,7 @@ export JAVA_OPTS="-Xmx820m -Xms820m"
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 function ontrailclone() {
+    mongod&
     export LC_ALL=C
     B=`date +%s`
     ssh ontrail@ontrail.net mongodump 
