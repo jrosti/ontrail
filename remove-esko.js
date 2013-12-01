e6bca06d41175a6661a645e530456ff8f59db775
jro <jari.rosti@gmail.com>
2013-12-02 00:25:57 +0200

    removed register form

--- remove-esko.js
+++ remove-esko.js
@@ -1,2 +1,6 @@
-db.onuser.remove({username: "Esko"});
-db.exercise.remove({user: "Esko"});
+db.onuser.remove({username: "Pantse"});
+db.exercise.remove({user: "Pantse"});
+
+db.onuser.remove({username: "Soironen"});
+db.exercise.remove({user: "Soironen"});
+
