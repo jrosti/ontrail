aef86af4e56cfd1d149e34403da63ce5320e0980
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-01-12 15:14:16 +0200

    Index.html uses require.js

---
+++ resources/public/m/js/app/ontrail.js
@@ -0,0 +1,12 @@
+define(["jquery", "bacon", "bacon.jquery", "jquery.cookie"], function($, Bacon, bjq) {
+
+    var OnTrail = {}
+
+    OnTrail.logout = function() {
+        document.location = "login.html"
+    }
+
+    if (!$.cookie("authUser")) {
+        OnTrail.logout()
+    }
+})
