191fb7232e5d1a438bd0626cc654361012fb7787
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-01-12 21:12:01 +0200

    Pager functionality implemented in bacon.js

--- resources/public/m/js/app/ontrail.js
+++ resources/public/m/js/app/ontrail.js
@@ -1,12 +1,13 @@
-define(["jquery", "bacon", "bacon.jquery", "ontrail/ex", "jquery.cookie"], function($, Bacon, bjq, ex) {
+define(["jquery", "bacon", "bacon.jquery", "ontrail/ex", "ontrail/list-ui", "jquery.cookie"], function($, Bacon, bjq, ex, list) {
     if (!$.cookie("authToken")) {
         document.location = "login.html"
         return {}
     }
 
     function renderEx(exercises) {
-        console.log(exercises)
+        return "<div style=\"min-height: 300px\">refreshed</div>"
     }
 
-    ex.exercises({}).onValue(renderEx)
+    var updates = list.create("#content", ex.exercises, renderEx)
+    updates.onValue(function() { console.log("fresh", arguments) })
 })
--- resources/public/m/js/ontrail/ex.js
+++ resources/public/m/js/ontrail/ex.js
@@ -1,9 +1,14 @@
-define(["jquery", "bacon", "bacon.jquery"], function($, Bacon, bjq) {
+define(["jquery", "lodash", "bacon", "bacon.jquery"], function($, _,    Bacon, bjq) {
 
-    return {
-        exercises: function(filter) {
-            return bjq.ajax("/rest/v1/ex-list-filter?" + $.param(filter) + "page=1")
-        }
+    var pager = function(fromUrl) {
+        return fromUrl.scan({page: 0}, function(val, url) { var pg = val.page + 1; return {page: pg, url: url + "&page=" + pg} }).skip(1).map(".url")
     }
 
+    return {
+        exercises: function(pagerStream) {
+            var filter = {}
+            var urlPager = pager(pagerStream.map("/rest/v1/ex-list-filter?" + $.param(filter)))
+            return urlPager.ajax()
+        }
+    }
 })
