fd32467a1602f9bab0acc5e62824e1f6ec88debd
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-01-12 15:44:12 +0200

    login enables and disables based on input

--- resources/public/m/js/app/login.js
+++ resources/public/m/js/app/login.js
@@ -1,5 +1,12 @@
 define(["jquery", "bacon", "bacon.jquery"], function($, Bacon, bjq) {
     var usernames = bjq.textFieldValue($("#username"))
+    var passwords = bjq.textFieldValue($("#password"))
 
-    $("#login").clickE().onValue(function(e) { console.log(e)} )
+    var logins = bjq.Model.combine({"username": usernames, "password": passwords})
+    logins.syncConverter = function(x) { return x || "" }
+
+    function shouldEnableLogin(login) { console.log("laa", login); return login.username.length == 0 || login.password.length == 0 }
+    logins.map(shouldEnableLogin).assign($("#login"), "attr", "disabled")
+
+    return {}
 })
