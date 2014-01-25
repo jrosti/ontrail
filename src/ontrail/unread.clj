2bc3682a336e1f008cb191163ef24355d042b18b
jro <jari.rosti@gmail.com>
2014-01-25 19:39:18 +0200

    unread result is limited to 30 exercises

--- src/ontrail/unread.clj
+++ src/ontrail/unread.clj
@@ -24,7 +24,7 @@
   (sort-by :lastModifiedDate time/after? (filter identity (map find-exercise (get-oids user)))))
 
 (defn comments-all [user]
-  (ex/decorate-results user (take 100 (get-unread-objs user))))
+  (ex/decorate-results user (take 30 (get-unread-objs user))))
 
 (defn is-own? [user ex]
   (or (= user (:user ex))
