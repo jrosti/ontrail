6771cbd64cb3222b376ae0d2ed18dfce4a8e43b4
jro <jari.rosti@gmail.com>
2014-02-23 13:45:01 +0200

    simplified test case

--- test/ontrail/test/mongerfilter.clj
+++ test/ontrail/test/mongerfilter.clj
@@ -9,19 +9,13 @@
   (is (= "42195" ((value-conversion-fun :title) "42195"))
       ":title is not converted"))
 
-;; (or-filter :k "a,b,c")
-;; => {"$or" [{:k "a"} {:k "b"} {:k "c"}]}
 (deftest create-or-filter
-  (let [or-test (or-filter :kw "a,b,c")
-        values (or-test "$or")]
-    (is (= 3 (count values))
-        "three distinct values are extracted from key-value pair")
-    (is (every? identity (map :kw values))
-        "every value contains keyword :kw")
-    (is (= #{"a" "b" "c"} (set (map :kw values)))
-        "values a, b, c are extracted from kw")))
+    (is (= {"$or" [{:k "a"} {:k "b"} {:k "c"}]} (or-filter :k "a,b,c"))
+        "comma separated values are applied to or"))
 
-(deftest monger-range-lt
+(deftest monger-ranges
   (is (= {:distance {"$lt" "42195"}} (to-monger-range identity :lt_distance "42195"))
-      "conversion to less than"))
+      "conversion to less than with identity mapper for value"))
+
+
 
