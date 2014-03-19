9cbdb2a099b15a0445e77f423f79a75542bab49e
jro <jari.rosti@gmail.com>
2014-03-19 22:43:42 +0200

    a release

--- project.clj
+++ project.clj
@@ -25,9 +25,7 @@
                 ]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler ontrail.core/app}
-  :run-aliases {:import [ontrail.heiaimport -main]
-                :create-user ontrail.user
-                :import-images [ontrail.imgimport -main]}
+  :aliases {"import" ["run" "-m" "ontrail.heiaimport/import-json"]}
   :aot  [ontrail.core]
   :main ontrail.core)
 
