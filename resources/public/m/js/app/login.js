9807378642d0facb5bf5d6fc4ed00a642345a9b9
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-05-08 23:45:32 +0300

    Use v1 login and manually set cookies instead.
    Does nothing until subscribed, therefore the listeners 
    in login.html

--- resources/public/m/js/app/login.js
+++ resources/public/m/js/app/login.js
@@ -1,9 +1,4 @@
-define(["jquery", "bacon", "bacon.jquery", "jquery.cookie"], function($, Bacon, bjq) {
-    if ($.cookie("authToken")) {
-        document.location = "index.html"
-        return {}
-    }
-
+define(["jquery", "lodash", "bacon", "bacon.jquery", "jquery.cookie"], function($, _, Bacon, bjq) {
     var usernames = bjq.textFieldValue($("#username"))
     var passwords = bjq.textFieldValue($("#password"))
 
@@ -13,5 +8,32 @@ define(["jquery", "bacon", "bacon.jquery", "jquery.cookie"], function($, Bacon, 
     function shouldEnableLogin(login) { return login.username.length == 0 || login.password.length == 0 }
     logins.map(shouldEnableLogin).assign($("#login"), "attr", "disabled")
 
-    return {}
+    function loginReq(params) {
+        return Bacon.$.lazyAjax( { type: "post", url: "/rest/v1/login", data: params } )
+    }
+
+    function logoutReq() {
+        return Bacon.$.lazyAjax( { type: "post", url: "/rest/v1/logout" })
+    }
+
+    function profileReq(username) {
+        return Bacon.$.lazyAjax( "/rest/v1/logged-ins" ).map(_.partial(_.extend, {username: username}))
+    }
+
+    function clearLoginCookie() {
+        $.cookie("authToken", null)
+        $.cookie("authUser", null)
+    }
+
+    function setLoginCookie(login) {
+        $.cookie("authToken", login.token, { expires: 365, path: "/" }  )
+        $.cookie("authUser", login.username, { expires: 365, path: "/" } )
+    }
+
+    var loginRequests = logins.sampledBy($("#login").clickE()).flatMapLatest(loginReq)
+    var loginErrors = loginRequests.errors().map({})
+    var loggedIns = loginRequests.filter(_.identity).doAction(setLoginCookie).map(".username").flatMapLatest(profileReq)
+    var loggedOuts = $("#logout").clickE().merge(loginErrors).flatMapLatest(logoutReq).doAction(clearLoginCookie)
+
+    return loggedIns.merge(loggedOuts)
 })
