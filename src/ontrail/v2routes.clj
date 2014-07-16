f9f95a31354a94ee89be38f3c58de209c5ead0f3
jro <jari.rosti@reaktor.fi>
2014-07-16 11:06:58 +0300

    refactored duplicate code in v2routes

--- src/ontrail/v2routes.clj
+++ src/ontrail/v2routes.clj
@@ -4,50 +4,47 @@
   (:require [ontrail.auth :as auth]
             [ontrail.user :as user]))
 
-(defroutes v2routes
+(defn logout [params headers]
+  (let [redirect-location (if-let [param-referer (:referer params)]
+                            param-referer
+                            (if-let [referer (headers "referer")] referer "/m/index.html"))]
+    {:status  301
+     :headers {"Content-Type" "text/html"
+               "Location"     redirect-location}
+     :cookies {"authToken" {:value "" :max-age 0 :path "/"}
+               "authUser"  {:value "" :max-age 0 :path "/"}}
+     :body    ""}))
+
+(defroutes
+  v2routes
 
   (POST "/rest/v2/login" {params :params headers :headers}
         (let [username (:username params)
               password (:password params)
-              redirect-location (if-let [param-referer (:referer params)] param-referer
-                                         (if-let [referer (headers "referer")] referer "/m/index.html"))]
+              redirect-location (if-let [param-referer (:referer params)]
+                                  param-referer
+                                  (if-let [referer (headers "referer")] referer "/m/index.html"))]
           (if (auth/authenticate username password)
             (let [case-user (user/get-case-user username)
                   authToken (auth/auth-token case-user)
                   authUser (:username (user/get-case-user username))]
-              {:status 301
+              {:status  301
                :headers {"Content-Type" "text/html"
-                         "Location" redirect-location}
-               :cookies {"authToken" {:value authToken :max-age (* 60 60 24 2 365) :path "/" }
-                         "authUser" {:value authUser :max-age  (* 60 60 24 2 365) :path "/" }}
-               :body ""
+                         "Location"     redirect-location}
+               :cookies {"authToken" {:value authToken :max-age (* 60 60 24 2 365) :path "/"}
+                         "authUser"  {:value authUser :max-age (* 60 60 24 2 365) :path "/"}}
+               :body    ""
                })
-            {:status 301
-             :headers {"Content-Type" "text/html"
-                       "Location" (str redirect-location "?status=login_failed")
+            {:status  301
+             :headers {"Content-Type"     "text/html"
+                       "Location"         (str redirect-location "?status=login_failed")
                        "X-Ontrail-Status" "login-failed"}
-             :body ""
+             :body    ""
              })))
 
   (GET "/rest/v2/logout" {params :params headers :headers}
-        (let [redirect-location (if-let [param-referer (:referer params)] param-referer
-                                         (if-let [referer (headers "referer")] referer "/m/index.html"))]
-          {:status 301
-           :headers {"Content-Type" "text/html"
-                     "Location" redirect-location}
-           :cookies {"authToken" {:value "" :max-age 0 :path "/"}
-                     "authUser" {:value "" :max-age  0 :path "/"}}
-           :body ""}))
+       (logout params headers))
 
   (POST "/rest/v2/logout" {params :params headers :headers}
-        (let [redirect-location (if-let [param-referer (:referer params)] param-referer
-                                         (if-let [referer (headers "referer")] referer "/m/index.html"))]
-          {:status 301
-           :headers {"Content-Type" "text/html"
-                     "Location" redirect-location}
-           :cookies {"authToken" {:value "" :max-age 0 :path "/"}
-                     "authUser" {:value "" :max-age  0 :path "/"}}
-           :body ""}))
+        (logout params headers)))
 
-  ;; routes
-  )
