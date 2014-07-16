3568cf90eb81d584eb3a81ecfebf5fb048043321
jro <jari.rosti@reaktor.fi>
2014-07-16 13:02:49 +0300

    fixed call to undefined function

--- create-index.js
+++ create-index.js
@@ -1,12 +1,12 @@
-
-
-db.exercise.ensureIndex({creationDate: 1})
-db.exercise.ensureIndex({lastModifiedDate: 1})
-db.exercise.ensureIndex({user: 1}) 
-db.exercise.ensureIndex({sport: 1})
-db.exercise.ensureIndex({duration: 1}) 
-db.exercise.ensureIndex({distance : 1})
-db.exercise.ensureIndex({tags: 1})
-db.groups.ensureIndex({name: 1})
-db.groups.ensureIndex({users: 1})
-db.nccache.ensureIndex({u: 1})
+db.exercise.ensureIndex({creationDate: 1});
+db.exercise.ensureIndex({creationDate: 1, lastModifiedDate: 1});
+db.exercise.ensureIndex({lastModifiedDate: 1, creationDate: 1});
+db.exercise.ensureIndex({lastModifiedDate: 1});
+db.exercise.ensureIndex({user: 1}) ;
+db.exercise.ensureIndex({sport: 1});
+db.exercise.ensureIndex({duration: 1}) ;
+db.exercise.ensureIndex({distance : 1});
+db.exercise.ensureIndex({tags: 1});
+db.groups.ensureIndex({name: 1});
+db.groups.ensureIndex({users: 1});
+db.nccache.ensureIndex({u: 1});
