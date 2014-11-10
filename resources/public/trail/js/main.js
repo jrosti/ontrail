070fe6a77ed1871e0ba627d0b10e52f855386a29
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-11-10 23:23:06 +0200

    add timeago plugin and render default text for date.

--- resources/public/trail/js/main.js
+++ resources/public/trail/js/main.js
@@ -3,11 +3,14 @@ var $ = require("jquery")
 var _ = require("lodash")
 var MediumEditor = require("medium-editor")
 var dialog = require("./editor/dialog")
+var moment = require('moment')
+window.jQuery = $
+var timeago = require('jquery-timeago')
+var Rx = require('rx')
 
 var editorOpts = {
   cleanPastedHTML: true,
-    placeholder: "Kerro jotain lenkistäsi ..."
-
+  placeholder: "Kerro jotain lenkistäsi ..."
 }
 
 var titleEditorOpts = {
@@ -20,9 +23,18 @@ var titleEditorOpts = {
   placeholder: "Naseva otsikko lenkillesi!"
 }
 
+var dates = new Rx.Subject()
+
 $(document).ready(function() {
   var titleEditor = new MediumEditor("#ex-title", titleEditorOpts) // instantiate content editor
   var contentEditor = new MediumEditor(".editable", editorOpts) // instantiate content editor
+
+  dates.subscribe(function(dateStr) {
+    $("#ex-date").attr("title", dateStr).timeago()
+  })
+
+  var now = moment().utc().format("YYYY-MM-DD\THH:mm:SS") + "Z";
+  dates.onNext(now)
 
   var dlg = dialog.create()
 })
