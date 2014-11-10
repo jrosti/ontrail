a35febc9c89acad5492b82aa21baa853f8de9af3
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-11-10 20:49:31 +0200

    Make also caption editable

--- resources/public/trail/js/main.js
+++ resources/public/trail/js/main.js
@@ -4,9 +4,25 @@ var _ = require("lodash")
 var MediumEditor = require("medium-editor")
 var dialog = require("./editor/dialog")
 
+var editorOpts = {
+  cleanPastedHTML: true,
+    placeholder: "Kerro jotain lenkistäsi ..."
+
+}
+
+var titleEditorOpts = {
+  cleanPastedHTML: true,
+  disableReturn: true,
+  disableDoubleReturn: true,
+  firstHeader: 'h1',
+  secondHeader: 'h2',
+  disableToolbar: true,
+  placeholder: "Naseva otsikko lenkillesi!"
+}
+
 $(document).ready(function() {
-  var contentEditor = new MediumEditor(".editable") // instantiate content editor
+  var titleEditor = new MediumEditor("#ex-title", titleEditorOpts) // instantiate content editor
+  var contentEditor = new MediumEditor(".editable", editorOpts) // instantiate content editor
 
   var dlg = dialog.create()
-
 })
