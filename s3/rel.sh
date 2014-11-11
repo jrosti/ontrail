032e6013cb7e522903528a4ff5dfc938be93453a
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-11-11 22:26:11 +0200

    Add file upload server.

---
+++ s3/rel.sh
@@ -0,0 +1,2 @@
+lein uberjar
+scp target/s3-0.0.1-standalone.jar ontrail@ontrail.net:s3
---
+++ s3/src/s3/auth.clj
@@ -0,0 +1,36 @@
+(ns s3.auth
+  (:use [s3 user crypto]
+        [clojure.string :only [split]]))
+
+(def #^{:private true} logger (org.slf4j.LoggerFactory/getLogger (str *ns*)))
+
+(defn authenticate [username password]
+  (let [user (get-case-user username)
+        is-match (and (not (= user nil)) (password-match? password (:passwordHash user)))]
+    is-match))
+
+(defn hash-part [password-hash]
+  (if password-hash
+    (last (.split password-hash "\\$"))
+    ""))
+
+(defn auth-token [user]
+  (encrypt (str (:username user) "|" (hash-part (:passwordHash user)))))
+
+(defn user-from-token [token]
+  (let [[username, passwordHash] (split (decrypt token) #"\|")]
+    {:username username :passwordHash passwordHash}))
+
+(defn valid-auth-token? [token]
+  (if (not token)
+    false
+    (let [from-token (user-from-token token)
+          user (if from-token (get-user (:username from-token)) {})]
+        (and user (= (:passwordHash from-token) (hash-part (:passwordHash user)))))))
+
+(defn user-from-cookie [cookies]
+  (try
+      (:username (user-from-token (:value (cookies "authToken"))))
+      (catch Exception exception
+        "nobody")))
+    
