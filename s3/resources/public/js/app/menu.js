e1d43bf5e83a428c997f74da95a319a9ab91c726
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-11-12 23:50:23 +0200

    Add menu to index.html as well and make it obey logged-in state

--- s3/resources/public/js/app/menu.js
+++ s3/resources/public/js/app/menu.js
@@ -3,16 +3,13 @@ var Rx = require("rx")
 var cookie = require("cookie-cutter")
 require("rx-jquery")
 
-exports.create = function () {
-  $("#show-menu").onAsObservable("click").subscribe(function() {
-    $("body").toggleClass("menu-collapsed")
-    $("#side-menu .pure-menu").toggleClass("pure-menu-open")
-  })
+$("#show-menu").onAsObservable("click").subscribe(function() {
+  $("body").toggleClass("menu-collapsed")
+  $("#side-menu .pure-menu").toggleClass("pure-menu-open")
+})
 
-  $("#logout").onAsObservable("click").subscribe(function() {
-    cookie.set("authUser", null, { expires: new Date(0) })
-    cookie.set("authToken", null, { expires: new Date(0) })
-    document.location = "/"
-  })
-
-}
+$("#logout").onAsObservable("click").subscribe(function() {
+  cookie.set("authUser", null, { expires: new Date(0) })
+  cookie.set("authToken", null, { expires: new Date(0) })
+  document.location = "/"
+})
