5fed60beb8bcc16f1584c11b9842e7b7d72893bf
Jari Rosti <jari.rosti@reaktor.fi>
2014-11-13 00:21:59 +0200

    logged-in service

--- s3/src/s3/otroutes.clj
+++ s3/src/s3/otroutes.clj
@@ -2,12 +2,15 @@
   (:require [ring.middleware.multipart-params :as mp]
             [clojure.edn :as edn]
             [taoensso.timbre :as timbre])
-  (:use [s3 webutil formats parser]
+  (:use [s3 webutil formats parser loggedin auth]
         [compojure.core :only [defroutes GET POST DELETE ANY context]]))
 
 (timbre/refer-timbre)
 
 (defroutes ontrail-routes
+
+  (GET "/trail/rest/logged-ins" {cookies :cookies} (json-response (params (user-from-cookie cookies))))
+
   (GET "/trail/rest/validate/time/:time" [time]
       (let [duration (to-human-time (parse-duration time))]
         (if (= "" duration)
