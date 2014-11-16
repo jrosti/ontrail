1a75f69aa38e1bbcd656f0cdf85c7a7cc0106f7b
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-11-16 20:53:46 +0200

    add zalendar

---
+++ s3/resources/public/js/zalendar/zalendar.js
@@ -0,0 +1,59 @@
+var _ = require("lodash")
+var moment = require("moment")
+var render = require("./render").renderWeek
+
+var defaultOpts = {
+  styles: {
+    week: "pure-g pure-u-1",
+    day: "pure-u-1-8",
+  },
+  monthData: function(month) { return month % 2 }
+}
+
+function zalendar(el, inOpts) {
+  var opts = _.merge({}, defaultOpts, inOpts)
+  var now = moment();
+
+  var $el = document.getElementById(el)
+  var beginningOfMonth = moment(now).date(1).isoWeekday(1)
+
+  // initial fill
+  var firstWeek = beginningOfMonth.subtract(2, 'weeks')
+  var lastWeek = moment(firstWeek)
+
+  function appendWeek() {
+    _.each(render(lastWeek, opts), function(child) {
+      console.log("laa2", child)
+      $el.appendChild(child)
+    })
+    lastWeek.add(1, "week")
+  }
+
+  function prependWeek() {
+    firstWeek.subtract(1, "week")
+    var height = $el.scrollHeight;
+    var top = $el.scrollTop;
+    _(render(firstWeek, opts)).reverse().each(function(child) {
+      $el.insertBefore(child, $el.children[0])
+    })
+    $el.scrollTop = top + ($el.scrollHeight - height)
+  }
+
+  while($el.scrollHeight < $el.offsetHeight)
+    appendWeek()
+  prependWeek()
+
+  $today = document.querySelector("#" + el + " .today")
+  var lineHeight = $today.offsetHeight
+  $el.scrollTop = lineHeight
+
+  $el.onscroll = function() {
+    if($el.scrollTop + $el.offsetHeight - $el.scrollHeight <= lineHeight)
+      appendWeek()
+    if($el.scrollTop < lineHeight)
+      prependWeek()
+  }
+}
+
+
+exports.create = zalendar
