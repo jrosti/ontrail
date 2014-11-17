34bdc61d52996e7a64afc63b47fb211cdb9e0b8a
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2014-11-17 16:52:37 +0200

    add “edit” button to individual blog page.

--- s3/resources/public/js/entry.js
+++ s3/resources/public/js/entry.js
@@ -1,8 +1,18 @@
 var $ = require("jquery")
+var _ = require("lodash")
 
 $(document).ready(function() {
   require("./app/menu")
-  require("./app/user") // initialize logged-in state
-  require("./blog/entry").populate()
+  var user = require("./app/user") // initialize logged-in state
+  var entry = require("./blog/entry").populate()
+
+  entry.combineLatest(user.auths, function(a, b) { return [a,b] } ).subscribe(function(params) {
+    var entry = params[0]
+    var user = params[1].user
+    if (entry.user == user) {
+      $("#edit-menu").show()
+      $("#edit-button").onAsObservable('click').subscribe(function() { document.location = "/edit/" + entry.id })
+    }
+  })
 })
 
