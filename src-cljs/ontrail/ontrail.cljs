f4413ec4e7ac5050ced295abf741a45fdb953970
Jari Rosti <jari.rosti@reaktor.fi>
2014-12-29 22:27:01 +0200

    joulu

--- src-cljs/ontrail/ontrail.cljs
+++ src-cljs/ontrail/ontrail.cljs
@@ -5,32 +5,32 @@
 (def time-color (atom "#f34"))
 
 (defn update-time [time]
-      ;; Update the time every 1/10 second to be accurate...
-      (js/setTimeout #(reset! time (js/Date.)) 100))
+  ;; Update the time every 1/10 second to be accurate...
+  (js/setTimeout #(reset! time (js/Date.)) 100))
 
 (defn greeting [message]
-      [:h1 message])
+  [:h1 message])
 
 (defn clock []
-      (update-time timer)
-      (let [time-str (-> @timer .toTimeString (clojure.string/split " ") first)]
-           [:div.example-clock
-            {:style {:color @time-color}}
-            time-str]))
+  (update-time timer)
+  (let [time-str (-> @timer .toTimeString (clojure.string/split " ") first)]
+    [:div.example-clock
+     {:style {:color @time-color}}
+     time-str]))
 
 (defn color-input []
-      [:div.color-input
-       "Time color: "
-       [:input {:type "text"
-                :value @time-color
-                :on-change #(reset! time-color (-> % .-target .-value))}]])
+  [:div.color-input
+   "Time color: "
+   [:input {:type "text"
+            :value @time-color
+            :on-change #(reset! time-color (-> % .-target .-value))}]])
 
 (defn simple-example []
-      [:div
-       [greeting "Hello world, it is now"]
-       [clock]
-       [color-input]])
+  [:div
+   [greeting "Hello world, it is now"]
+   [clock]
+   [color-input]])
 
 (defn ^:export run []
-      (reagent/render-component (fn [] [simple-example])
-                                (js/$ "#foo")))
+  (reagent/render-component (fn [] [simple-example])
+                            (.getElementById js/document "root")))
