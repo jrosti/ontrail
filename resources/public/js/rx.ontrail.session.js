0cbb05945ef477096eec9cf663e5f3e1f2099253
Jari Rosti <jari.rosti@reaktor.fi>
2015-03-15 18:09:13 +0200

    cookie setting to subdomain

--- resources/public/js/rx.ontrail.session.js
+++ resources/public/js/rx.ontrail.session.js
@@ -3,7 +3,14 @@
   var Session = function() {}
   Session.prototype.create = function(logins, logouts) {
     return rx.create(function(observer) {
-      logins.subscribe(function(login) { $.cookie("authToken", login.token, { expires: 365, domain: '.ontrail.net' }  ); $.cookie("authUser", login.username, { expires: 365, domain: '.ontrail.net' } ); observer.onNext(login.username) } )
+      logins.subscribe(function(login) { 
+        $.cookie("authToken", login.token, { expires: 365 }  ); 
+        $.cookie("authUser", login.username, { expires: 365 } ); 
+
+        $.cookie("authToken", login.token, { expires: 365, domain: '.ontrail.net' }  ); 
+        $.cookie("authUser", login.username, { expires: 365, domain: '.ontrail.net' } ); 
+        observer.onNext(login.username) 
+      } )
       logouts.subscribe(function() { $.cookie("authToken", null); $.cookie("authUser", null); observer.onNext(null)})
       observer.onNext($.cookie("authUser")) // initialize with login state
       return function() {}
