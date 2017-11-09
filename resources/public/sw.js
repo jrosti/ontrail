a4841dc18b016f874d3f235c3b7d9bae4b0273a1
Petri Louhelainen <psalmi@iki.fi>
2017-11-09 22:50:32 +0000

    Add external resources to service worker pool

--- resources/public/sw.js
+++ resources/public/sw.js
@@ -18,11 +18,23 @@ self.addEventListener('install', function(e) {
                 "/css/pikaday.css",
                 "/css/material-trail.css",
 
+                // remote css -- TODO, replace with https
+                'http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css',
+                'http://fonts.googleapis.com/icon?family=Material+Icons',
+                'http://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
+
+                // remote fonts
+                'http://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/fonts/roboto/Roboto-Regular.woff2',
+                'http://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/fonts/roboto/Roboto-Medium.woff2',
+                'http://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/fonts/roboto/Roboto-Bold.woff2',
+                'http://fonts.gstatic.com/s/materialicons/v31/2fcrYFNaTjcS6g4U3t-Y5RV6cRhDpPC5P4GCEJpqGoc.woff',
+                'http://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/fonts/roboto/Roboto-Light.woff2',
+
                 // own hosted images
-                'http://localhost:8080/img/logo.png',
-                'http://localhost:8080/img/login_bg.png',
-                'http://localhost:8080/img/share.jpg',
-                'http://localhost:8080/img/loading.gif',
+                '/img/logo.png',
+                '/img/login_bg.png',
+                '/img/share.jpg',
+                '/img/loading.gif',
 
                 // own hosted js
                 'js/libs/modernizr-2.5.3.min.js',
@@ -43,7 +55,20 @@ self.addEventListener('install', function(e) {
                 'js/chart.js',
                 'js/pikaday.js',
                 'js/plugins.js?_=UID',
-                'js/ontrail.js?_=UID'
+                'js/ontrail.js?_=UID',
+
+                // remotely hosted js
+                'http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js?_=UID',
+                'http://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js',
+                'http://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.0/js.cookie.min.js',
+                'http://cdnjs.cloudflare.com/ajax/libs/jquery.address/1.6/jquery.address.min.js?strict=false',
+                'http://cdnjs.cloudflare.com/ajax/libs/ICanHaz.js/0.10.3/ICanHaz.min.js',
+                'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.1.0/lodash.min.js',
+                'http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.1/moment.min.js',
+                'http://cdnjs.cloudflare.com/ajax/libs/rxjs/2.3.24/rx.all.js',
+                'http://cdnjs.cloudflare.com/ajax/libs/rxjs-jquery/1.1.6/rx.jquery.min.js',
+                'http://cdn.jsdelivr.net/fastclick/1.0.6/fastclick.min.js',
+
             ])
         })
     )
